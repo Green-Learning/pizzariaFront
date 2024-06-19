8dda739d846022f56ae32ec8c36b66343387a1ff
Guilh <wanderfull.mystc@gmail.com>
2024-06-19 17:04:41 -0300

    login with keycloack

--- src/app/sistema/login/login.component.ts
+++ src/app/sistema/login/login.component.ts
@@ -1,9 +1,7 @@
-
-import { Login } from './login';
-import { Usuario } from './../../usuario/usuario';
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { LoginService } from './services/login.service';
+import { Login } from './login';
 
 @Component({
   selector: 'app-login',
@@ -11,31 +9,25 @@ import { LoginService } from './services/login.service';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  login: Login = new Login();
+  roteador = inject(Router);
+  loginService = inject(LoginService);
 
-login : Login = new Login();
-roteador = inject(Router);
-loginService = inject(LoginService);
+  constructor() {
+    this.loginService.removerToken();
+  }
 
-constructor() {
-  this.loginService.removerToken();
-}
-
-logar(){
-
-  this.loginService.logar(this.login).subscribe({
-    next: user => { // QUANDO DÁ CERTO
-      console.log(user);
-      this.loginService.addToken(user.token);
-      this.roteador.navigate(['/admin/pedidos']);
-    },
-    error: erro => { // QUANDO DÁ ERRO
-      alert('Exemplo de tratamento de erro/exception! Observe o erro no console!');
-      console.error(erro);
-    }
-  });
-
-}
-  
-
-
+  logar() {
+    this.loginService.logar(this.login).subscribe({
+      next: user => {
+        console.log(user);
+        this.loginService.addToken(user.token);
+        this.roteador.navigate(['/admin/pedidos']);
+      },
+      error: erro => {
+        alert('Exemplo de tratamento de erro/exception! Observe o erro no console!');
+        console.error(erro);
+      }
+    });
+  }
 }
--- src/app/sistema/login/services/login.service.ts
+++ src/app/sistema/login/services/login.service.ts
@@ -1,56 +1,82 @@
 import { Injectable, inject } from '@angular/core';
-import { Login } from '../login';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from '../user';
-import { HttpClient } from '@angular/common/http';
-import { jwtDecode, JwtPayload } from "jwt-decode";
+import { Login } from '../login';
+import {jwtDecode} from 'jwt-decode';
+import { JwtPayload } from 'jwt-decode';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
-
-  API: string = 'https://192.168.56.108/api/login';
+  API: string = 'https://localhost:443/api/login';
   http = inject(HttpClient);
+  router = inject(Router);
 
-  constructor() { }
-
+  constructor() {}
 
   logar(login: Login): Observable<User> {
     return this.http.post<User>(this.API, login);
   }
 
-  deslogar(): Observable<any> {
-    return this.http.get<any>(this.API + '/deslogar');
+  deslogar(): void {
+    localStorage.removeItem('token');
+    this.removeCookie('token');
+    this.router.navigate(['/login']);
   }
-
 
   addToken(token: string) {
     localStorage.setItem('token', token);
+    this.setCookie('token', token, 1); // Expires in 1 day
   }
 
   removerToken() {
     localStorage.removeItem('token');
+    this.removeCookie('token');
   }
 
-  getToken() {
-    return localStorage.getItem('token');
+  getToken(): string | null {
+    return localStorage.getItem('token') || this.getCookie('token');
   }
-
 
   jwtDecode() {
     let token = this.getToken();
     if (token) {
       return jwtDecode<JwtPayload>(token);
     }
-    return "";
+    return null;
   }
 
-  hasPermission(role: string) {
+  hasPermission(role: string): boolean {
     let user = this.jwtDecode() as User;
-    if (user.role == role)
-      return true;
-    else
-      return false;
+    return user?.role === role;
+  }
+
+  // Cookie methods
+  setCookie(name: string, value: string, days: number) {
+    let expires = "";
+    if (days) {
+      const date = new Date();
+      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+      expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+  }
+
+  getCookie(name: string): string | null {
+    const nameEQ = name + "=";
+    const ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+      if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+  }
+
+  removeCookie(name: string) {
+    document.cookie = name + '=; Max-Age=-99999999;';
   }
 }
